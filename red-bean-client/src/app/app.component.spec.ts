2561d423ba24908d4eb68466b40f025d60cfcd30
a3994288 <3994288@qq.com>
2021-09-24 17:38:35 -0400

    add css

--- red-bean-client/src/app/app.component.spec.ts
+++ red-bean-client/src/app/app.component.spec.ts
@@ -13,23 +13,4 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   });
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'red-bean-client'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('red-bean-client');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('red-bean-client app is running!');
-  });
 });
